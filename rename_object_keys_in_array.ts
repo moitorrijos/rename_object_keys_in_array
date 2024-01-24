5545adebb5beb05bb632e560aadb60f5f2034991
Juan Moises Torrijos <juanmtorrijos@gmail.com>
2024-01-24 11:13:32 -0500

    Create rename_object_keys_in_array.ts

---
+++ rename_object_keys_in_array.ts
@@ -0,0 +1,57 @@
+const persons = [
+  {
+    name: 'Juan Perez',
+    nationality: 'Panamanian',
+    country_of_birth: 'Panama',
+    id_passport: '1',
+    date_of_birth: '2000-04-10',
+  },
+  {
+    name: 'Maria Perez',
+    nationality: 'Panamanian',
+    country_of_birth: 'Panama',
+    id_passport: '2',
+    date_of_birth: '2000-04-10',
+  },
+  {
+    name: 'Ana Perez',
+    nationality: 'Panamanian',
+    country_of_birth: 'Panama',
+    id_passport: '3',
+    date_of_birth: '2000-04-10',
+  },
+]
+
+function renameKeysToTitleCase(arrObj: {}[]) {
+  function renameKey(obj: {}, old_key: string, new_key: string) {
+    if (old_key !== new_key) {
+      const descriptor = Object.getOwnPropertyDescriptor(obj, old_key)
+      if (descriptor) {
+        Object.defineProperty(obj, new_key, descriptor)
+        delete obj[old_key]
+      }
+    }
+  }
+
+  const toTitleCase = (str: string) => {
+    const strArray = str.toLowerCase().split('_')
+    const strArrayShift = strArray.shift()
+    const otherStringsArray = strArray
+      .map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      })
+      .join('')
+    return strArrayShift + otherStringsArray
+  }
+
+  arrObj.forEach((obj) => {
+    Object.keys(obj).forEach((key) => {
+      renameKey(obj, key, toTitleCase(key))
+    })
+  })
+
+  return arrObj
+}
+
+// Test it for yourself:
+console.log(renameKeysToTitleCase(persons))
